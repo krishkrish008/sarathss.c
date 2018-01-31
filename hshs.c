c7e680539fb827afe63176f714ef78283a1b1cb2
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-31 16:14:52 +0530

    hahaaa.c

---
+++ hshs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c;
+   printf("Enter value of a"); 
+   scanf("%d",&a);
+    for(b=1;b<=a;b++)
+    {
+printf("\nMULTIPLES are");   
+ c=b*5;
+printf("%d ",c);
+    }
+return 0;
+}
